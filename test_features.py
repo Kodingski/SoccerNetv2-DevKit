a3eb818d9438dcbf608aab13e781356386f1b57a
Kodingski <timschwarz@mail.de>
2022-03-23 10:27:54 +0100

    Extracted features of first test game (SGE-AUG)
    Extracted features of first test game (SGE-AUG)

---
+++ test_features.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 22 16:43:30 2022
+
+@author: timsc
+"""
+
+#%%imports
+import numpy as np
+
+#%%
+path = r'D:\Drive\Leiden\4. Semester\Internship\SoccerNetv2-DevKit\Data\features.npy'
+
+features = np.load(path)
