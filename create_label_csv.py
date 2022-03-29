b324255075c921f6420c2499dd5c06fc65dea57b
Kodingski <timschwarz@mail.de>
2022-03-29 16:40:01 +0200

    transformed labels to csv

--- create_label_csv.py
+++ create_label_csv.py
@@ -12,11 +12,12 @@ import pandas as pd
 #%%load
 # parse an xml file by name
 tree = ET.parse('Data/DFL-MAT-003CPF_full_video_labels_uniform_setpieces_2.5_0.4_1.0_214.5_4074.45.xml')
-
-#%%#%%make dictionary to later transform into .csv
-events_dict = {}
+lst = tree.findall('ALL_INSTANCES/instance')
 
 #%%
+#make dictionary to later transform into .csv
+events_dict = {}
+
 #counter to keep track of event time in trimmed video
 event_counter = 0
 #match_id = 
@@ -29,19 +30,19 @@ for event in lst:
     
     event_dict = {}
 
-    event_dict["start_raw"] = event.find('start').text
-    event_dict["end_raw"] = event.find('end').text
+    event_dict["start_time_snippet"] = event.find('start').text
+    event_dict["end_time_snippet"] = event.find('end').text
     
-    event_dict["id"] = event_counter
-    event_dict["start"] = starting_time
-    event_dict["end"] = starting_time + 2.5
+    event_id = event_counter+1
+    #event_dict["start"] = starting_time
+    #event_dict["end"] = starting_time + 2.5
     
     event_dict["event_attributes"] = []
     
 
     #initialiaze timesteps of actual events as none for the case none happened 
-    event_dict['timestep_raw'] = None
-    event_dict['timestep'] = None
+    #event_dict['timestep_raw'] = None
+    event_dict['event_timestamp'] = None
 
     
     #if an event exists in clip (and therefore also a timestamp), write in dict
@@ -49,8 +50,8 @@ for event in lst:
     for label in labels:
         if label.find("group").text == 'Event_Timestamp':
             #print(label.find('text').text)
-            event_dict['timestep_raw'] = label.find('text').text
-            event_dict['timestep'] = starting_time + (float(event_dict['timestep_raw']) - float(event_dict["start_raw"])) 
+            event_dict['event_timestamp'] = label.find('text').text
+            #event_dict['timestep'] = starting_time + (float(event_dict['timestep_raw']) - float(event_dict["start_raw"])) 
         
         if label.find("group").text in ["tackle_WinnerResult", 
                                            "Event_SubType"] :
@@ -63,18 +64,23 @@ for event in lst:
         event_dict["event_attributes"].append("Open")
     
     if event_type in ['Freekick', 'Corner']:
-        print('here')
         event_dict["event_attributes"].append(event_type)
         event_type = 'Play'        
 
-    event_dict["type"] = event_type
+    event_dict["event_type"] = event_type
 
     event_counter += 1
-    events_dict["annotations"].append(event_dict)
+    events_dict[event_id] = event_dict
 
 #%%transform to df and save as .csv
+#reorder
+
+
 
 df = pd.DataFrame.from_dict(data=events_dict, orient='index')
- 
+df = (df[['event_type', 'event_attributes', 
+         'start_time_snippet', 'end_time_snippet',
+         'event_timestamp']])
+
 df.to_csv('Data/labels_kaggle.csv', header=True)
 
