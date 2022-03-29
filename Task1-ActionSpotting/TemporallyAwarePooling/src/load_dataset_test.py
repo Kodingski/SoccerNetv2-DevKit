9db86c07608e43083fea864fd7755a388e7d7606
Kodingski <timschwarz@mail.de>
2022-03-29 17:57:18 +0200

    worked on labels

--- Task1-ActionSpotting/TemporallyAwarePooling/src/load_dataset_test.py
+++ Task1-ActionSpotting/TemporallyAwarePooling/src/load_dataset_test.py
@@ -84,20 +84,24 @@ feat_soccernet_torch = feats2clip(torch.from_numpy(feat_soccernet), stride=windo
 
 #feat_half2 = feats2clip(torch.from_numpy(feat_half2), stride=self.window_size_frame, clip_length=self.window_size_frame)
 
-#%%
-feat_game_torch = torch.from_numpy(feat_game)
 
 #%%
-# Load labels
-labels = json.load(open(os.path.join(self.path, game, self.labels)))
+# Load soccernet labels
+labels_soccernet = json.load(open(r'england_epl\2014-2015\2015-02-21 - 18-00 Crystal Palace 1 - 2 Arsenal\Labels-v2.json'))
 
-label_half1 = np.zeros((feat_game.shape[0], self.num_classes+1))
-#label_half1[:,0]=1 # those are BG classes
-#label_half2 = np.zeros((feat_half2.shape[0], self.num_classes+1))
-#label_half2[:,0]=1 # those are BG classes
+#%%recreate soccernet labels 
+version = 2
+dict_event = EVENT_DICTIONARY_V2
+game_labels = []
+num_classes = len(dict_event)
+
+label_half1 = np.zeros((feat_game.shape[0], num_classes+1))
+label_half1[:,0]=1 # those are BG classes
+label_half2 = np.zeros((feat_game.shape[0], num_classes+1))
+label_half2[:,0]=1 # those are BG classes
 
 
-for annotation in labels["annotations"]:
+for annotation in labels_soccernet["annotations"]:
 
     time = annotation["gameTime"]
     event = annotation["label"]
@@ -114,28 +118,36 @@ for annotation in labels["annotations"]:
         elif "soccer" in event: label = 2
         else: continue
     elif version == 2:
-        if event not in self.dict_event:
+        if event not in dict_event:
             continue
-        label = self.dict_event[event]
+        label = dict_event[event]
 
     # if label outside temporal of view
-    if half == 1 and frame//self.window_size_frame>=label_half1.shape[0]:
+    if half == 1 and frame//window_size_frame>=label_half1.shape[0]:
         continue
-    if half == 2 and frame//self.window_size_frame>=label_half2.shape[0]:
+    if half == 2 and frame//window_size_frame>=label_half2.shape[0]:
         continue
 
     if half == 1:
-        label_half1[frame//self.window_size_frame][0] = 0 # not BG anymore
-        label_half1[frame//self.window_size_frame][label+1] = 1 # that's my class
+        label_half1[frame//window_size_frame][0] = 0 # not BG anymore
+        label_half1[frame//window_size_frame][label+1] = 1 # that's my class
 
     if half == 2:
-        label_half2[frame//self.window_size_frame][0] = 0 # not BG anymore
-        label_half2[frame//self.window_size_frame][label+1] = 1 # that's my class
+        label_half2[frame//window_size_frame][0] = 0 # not BG anymore
+        label_half2[frame//window_size_frame][label+1] = 1 # that's my class
 
-self.game_feats.append(feat_game)
 #self.game_feats.append(feat_half2)
-self.game_labels.append(label_game)
+game_labels.append(label_half1)
+game_labels.append(label_half2)
 #self.game_labels.append(label_half2)
 
-self.game_feats = np.concatenate(self.game_feats)
-self.game_labels = np.concatenate(self.game_labels)
+game_labels = np.concatenate(game_labels)
+
+#%%load sportec lables
+labels = json.load(open(r'Data/SGE_FCA_annotations_snippets.json'))
+
+
+##next: Recreate their one hot encoding on sportec data
+
+
+#%%do it with sportec labels
